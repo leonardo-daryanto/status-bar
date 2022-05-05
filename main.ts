f5c8a66e5b3ee325b983f97c8782f889bb9da505
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-05-05 09:11:27 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
 
+namespace statusBar {
+    /**
+     * for creating the status bar
+     */
+    //% block
+    export function createStatusBar(width: number, height: number): void {
+        let statusbar2 = statusbars.create(width, height, StatusBarKind.Health)
+    }
+    /**
+     * After creating a status bar, use this block
+     */
+    //% block
+    export function setStatusBarTo(num: number): void {
+        let statusbar22 = statusbars.create(100, 40, StatusBarKind.Health)
+        statusbar22.value = num
+    }
+    /**
+     *  change the status bar
+     */
+    //% block
+    export function changeStatusBarTo(num: number): void {
+        let statusbar23 = statusbars.create(100, 40, StatusBarKind.Health)
+        statusbar23.value += num
+    }
+}
